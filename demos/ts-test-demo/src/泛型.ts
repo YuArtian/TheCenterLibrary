b739b909a7622b3b7e3e25b95308415a66aec73c
zhaoxueyu <312364705@qq.com>
2023-01-08 16:09:47 +0800

    add ts

--- demos/ts-test-demo/src/泛型.ts
+++ demos/ts-test-demo/src/泛型.ts
@@ -9,3 +9,8 @@ identity<number, string>(1, '2')
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Parameters1<T extends (...args: any) => any> = T extends (...args: infer P) =>  any ? P : never
+
+
+type T1 = Parameters<(s: string) => void>
+type T2 = Parameters<(s: string, s2: number) => void>
+
