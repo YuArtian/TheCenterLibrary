5caa8b4a5ff97de9cc5f58050b1bae4127d3c667
zhaoxueyu <312364705@qq.com>
2023-02-28 23:34:05 +0800

    no message

--- demos/vue3/vue3-ts-demo/src/App.vue
+++ demos/vue3/vue3-ts-demo/src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+const msg = ref('You did it!')
+setTimeout(() => {
+  msg.value = 'You did it again!!'
+}, 2000)
 </script>
 
 <template>
@@ -8,7 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld :msg="msg" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
--- demos/vue3/vue3-ts-demo/src/components/HelloWorld.vue
+++ demos/vue3/vue3-ts-demo/src/components/HelloWorld.vue
@@ -1,12 +1,42 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+<!-- <script setup lang="ts">
+// defineProps<{
+//   msg: string
+// }>()
+</script> -->
+
+<script lang="ts">
+import { defineComponent, onMounted, onUpdated, getCurrentInstance } from 'vue';
+
+
+export default defineComponent({
+  props: ['msg'],
+  setup(props: any) {
+    const vm = getCurrentInstance()
+    console.log('props', props)
+    onMounted(() => {
+      console.log('mounted-1')
+    })
+    onUpdated(() => {
+      console.log('onUpdated-1')
+      console.log('vm', vm)
+      // vm.data.msg2= 'update'
+    })
+    return {
+      // msg: 'xxxxx'
+      // ...props
+      msg2: props.msg + '2222'
+    }
+  },
+  mounted() {
+      console.log('mounted-2')
+  },
+})
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
+    <h1 class="green">{{ msg2 }}</h1>
     <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
