9e86f21268a87b7d3895b10d3b6510e8fd5c4a1d
yuartian <312364705@qq.com>
2023-04-25 16:29:01 +0800

    nextTick原理

---
+++ demos/vue2/vue2-demo/src/views/Nexttick.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div id="now" ref="test1">{{now}}</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'next-tick',
+  data() {
+    return {
+      now: 'default'
+    }
+  },
+  mounted() {
+    console.log('mounted -1')
+    this.now = 'mounted1'
+    this.$nextTick(() => {
+      console.log('nextTick-1', this.$refs.test1.innerHTML)
+      this.now = 'nextTick1'
+    })
+    this.$nextTick(() => {
+      console.log('nextTick-2', this.$refs.test1.innerHTML)
+      this.now = 'nextTick2'
+    })
+    console.log('mounted -2')
+    this.now = 'mounted2'
+    this.$nextTick(() => {
+      console.log('nextTick-3', this.$refs.test1.innerHTML)
+      this.now = 'nextTick3'
+    })
+  }
+}
+</script>
+<style></style>
