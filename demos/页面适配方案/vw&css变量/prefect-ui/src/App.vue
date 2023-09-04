6b8269ffb2dd0270b4c372da5b821f86e3eebef2
Ame <312364705@qq.com>
2023-09-04 22:20:06 +0800

    update

---
+++ demos/页面适配方案/vw&css变量/prefect-ui/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main">
+    <div class="content">
+      我是一段文本
+      我是一段文本
+      我是一段文本
+      我是一段文本
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+
+</style>
